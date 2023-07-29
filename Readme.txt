d4f7ddc94fbb935efc455e05d2b6a5d72890ccd3
Mustafa Akgül <46718026+mustafaakgul@users.noreply.github.com>
2023-07-29 12:40:57 +0300

    Initial commit

---
+++ Readme.txt
@@ -0,0 +1,7 @@
+# package-iot
+
+Name : package-iot
+Version : 1.0
+Description : Can be used for IoT devices
+Packages : package-iot
+Author_email : mustafaakguldev@icloud.com
---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='package-iot',
+      version='1.0',
+      description='Calculate and visuallize, Gaussian, Binomial and Normal distributions',
+      author='Mustafa Akgül',
+      author_email='mustafaakguldev@icloud.com',
+      packages=['package-iot'],
+      zip_safe=False)
